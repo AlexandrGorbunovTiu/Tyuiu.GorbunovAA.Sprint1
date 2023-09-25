4965ed1ddfa5c6ab3b94f8a45e394c81067408cc
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-09-25 11:13:15 +0500

    Task4

---
+++ Tyuiu.GorbunovAA.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorbunovAA.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(res, 1,111);
+
+        }
+    }
+}
---
+++ Tyuiu.GorbunovAA.Sprint1.Task4.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorbunovAA.Sprint1.Task4.V1.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint1.Task4.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 │ Выполнил: Горбунов А.А. │ ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Вынолнил: Горбунов Александр Александрович │ ИИПб-23-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ                                                                 *");
+            Console.WriteLine("* Написать программу для вычисления выражения 1 / (x + 2) ** 2,           *");
+            Console.WriteLine("* округленного до 3 знаков после запятой                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int x;
+
+            Console.WriteLine("Введите значение Х:");
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / (x + 2) ** 2 = " + ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
