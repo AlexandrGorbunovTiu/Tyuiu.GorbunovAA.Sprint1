bfea22d3daa29f0ba70779b1ffe0bf4094b6ea94
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-09-26 16:00:45 +0500

    Task5

---
+++ Tyuiu.GorbunovAA.Sprint1.Task5.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorbunovAA.Sprint1.Task5.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorbunovAA.Sprint1.Task5.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d029333a-9c20-468e-9d63-b07285c919d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GorbunovAA.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorbunovAA.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.GorbunovAA.Sprint1.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 │ Выполнил: Горбунов А.А. │ ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Вынолнил: Горбунов Александр Александрович │ ИИПб-23-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ                                                                 *");
+            Console.WriteLine("* Написать программу для вычисления расстояния между двумя точками,       *");
+            Console.WriteLine("* ответ привести к целому                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double x1; double y1; double x2; double y2;
+
+            Console.WriteLine("Введите ординату первой точки:");
+
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите абциссу первой точки:");
+
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите ординату второй точки:");
+
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите абциссу второй точки:");
+
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Расстояние между двумя точками - " + ds.DistanceBetweenDots(x1, y1, x2, y2));
+            Console.ReadKey();
+        }
+    }
+}
